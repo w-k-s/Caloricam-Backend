cb638c7b2596065f7457f29a8ed821229c14dbca
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2023-07-27 12:53:41 +0400

    Use JNDI for connection pooling

--- src/main/java/com/wks/calorieapp/utils/DatabaseUtils.java
+++ src/main/java/com/wks/calorieapp/utils/DatabaseUtils.java
@@ -1,5 +1,8 @@
 package com.wks.calorieapp.utils;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,87 +13,46 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
-public class DatabaseUtils
-{
-    private static final boolean appIsDeployed = true;
-    private static final String PARAM_DRIVER = "driver";
-    private static final String PARAM_URL = "url";
-    private static final String PARAM_USERNAME = "user";
-    private static final String PARAM_PASSWORD = "password";
-    private static final String DB_PROPERTIES_FILE = appIsDeployed ? "web_db.properties" : "local_db.properties";
-
+public class DatabaseUtils {
     private static Connection connection = null;
 
-    public static Connection getConnection()
-    {
-	if (connection != null) return connection;
+    public static Connection getConnection() {
+        if (connection != null) return connection;
 
-	try
-	{
-	    Properties dbProperties = new Properties();
-	    InputStream input = DatabaseUtils.class.getClassLoader().getResourceAsStream(DB_PROPERTIES_FILE);
-	    dbProperties.load(input);
-	    String url = "";
-	    if (appIsDeployed)
-	    {
-		url = "jdbc:mysql://127.12.26.130:3306/uploadte?autoReconnect=true";
-	    } else
-	    {
-		url = dbProperties.getProperty(PARAM_URL);
-	    }
-	    String driver = dbProperties.getProperty(PARAM_DRIVER);
-	    String username = dbProperties.getProperty(PARAM_USERNAME);
-	    String password = dbProperties.getProperty(PARAM_PASSWORD);
+        try {
+            InitialContext ctx = new InitialContext();
+            DataSource ds  = (DataSource) ctx.lookup("java:/comp/env/jdbc/main");
+            connection = ds.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 
-	    Class.forName(driver);
-	    connection = DriverManager.getConnection(url, username, password);
-	} catch (ClassNotFoundException e)
-	{
-	    e.printStackTrace();
-	} catch (SQLException e)
-	{
-	    e.printStackTrace();
-	} catch (FileNotFoundException e)
-	{
-	    e.printStackTrace();
-	} catch (IOException e)
-	{
-	    e.printStackTrace();
-	}
-	return connection;
+    public static void close(Connection connection) {
+        close(connection, null, null);
     }
-    
-    public static void close(Connection connection)
-    {
-	close(connection,null,null);
+
+    public static void close(Statement statement) {
+        close(null, statement, null);
     }
-    
-    public static void close(Statement statement)
-    {
-	close(null,statement,null);
+
+    public static void close(ResultSet result) {
+        close(null, null, result);
     }
-    
-    public static void close(ResultSet result)
-    {
-	close(null,null,result);
+
+    public static void close(Statement statement, ResultSet result) {
+        close(null, statement, result);
     }
-    
-    public static void close(Statement statement, ResultSet result)
-    {
-	close(null,statement,result);
-    }
-    
-    public static void close(Connection connection, Statement statement , ResultSet result)
-    {
-	try
-	{
-	    if(connection != null) connection.close();
-	    if (statement != null) statement.close();
-	    if (result != null) result.close();
-	} catch (SQLException e)
-	{
-	    
-	    e.printStackTrace();
-	}
+
+    public static void close(Connection connection, Statement statement, ResultSet result) {
+        try {
+            if (connection != null) connection.close();
+            if (statement != null) statement.close();
+            if (result != null) result.close();
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
     }
 }
